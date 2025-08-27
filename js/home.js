9675cb62b1a76764fadeffb668614a3e3976ef20
Mdshirajul9081 <shirajul15-2876@diu.edu.bd>
2025-08-27 07:51:06 +0600

    added the balence and current balance update

--- js/home.js
+++ js/home.js
@@ -7,10 +7,28 @@ document.getElementById("add-money-number").addEventListener("click",function(ev
     console.log('this is my add money button')
 
     // get the add money input value
-    const addMoneyInput=document.getElementById('input-add-money').value ;
+    const addMoneyInputElement=document.getElementById('input-add-money');
+    const addMoneyInput=addMoneyInputElement.value ;
+    // i get the input vlaue the its the empty
+    addMoneyInputElement.value=" ";
     console.log(addMoneyInput)
 
     // get the pin number
     const inputPin=document.getElementById("input-pin").value ;
     console.log(inputPin)
+    if(inputPin==="12345"){
+        console.log('update the input balance')
+        //   added the balance  
+        const balance=document.getElementById('current-balance').innerText
+        console.log(balance)
+        // convert the value string to number 
+        const balanceInput=parseFloat(balance)
+        const addMoney=parseFloat(addMoneyInput)
+        // update the main value
+        const newBalance=balanceInput+addMoney
+        console.log(newBalance)
+    }
+    else{
+        alert('send the currect pin number')
+    }
 })

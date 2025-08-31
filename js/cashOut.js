791a2b6fd98e1480939e49499fd81f78fcb0ff85
Mdshirajul9081 <shirajul15-2876@diu.edu.bd>
2025-08-31 13:19:11 +0600

    added the cashout form and js

--- js/cashOut.js
+++ js/cashOut.js
@@ -1,4 +1,21 @@
-document.getElementById("add-money-number").addEventListener("click",function(event){
+document.getElementById("add-cash-out").addEventListener("click",function(event){
     event.preventDefault()
-    console.log('this is the cash out money')
+    // console.log('this is the cash out money')
+    const addCashOut=document.getElementById("cashout-money").value ;
+    const cashOut=parseFloat(addCashOut)
+    // console.log(addCashOut)
+    const cashOutPin=document.getElementById("cashOut-pin").value 
+    // console.log(cashOutPin)
+    if(cashOutPin==="12345"){
+        const currentBalance=document.getElementById("current-balance").innerText;
+        const balance=parseFloat(currentBalance)
+        // main balance -cashout balance
+        const newBalance=balance-cashOut;
+        
+        // update ui value 
+        document.getElementById('current-balance').innerText=newBalance;
+    }
+    else{
+        alert("failde this pin please give this right pin number ")
+    }
 })
